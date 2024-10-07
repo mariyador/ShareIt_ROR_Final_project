c59d85f3e50a138ac3fa72a2190b39e78669a0bf
mariyador <aniknorod@gmail.com>
2024-10-06 23:27:37 -0400

    registration, login

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,8 @@
-import { Application } from "@hotwired/stimulus"
+import "@hotwired/turbo-rails";
+import { Application } from "@hotwired/stimulus";
 
-const application = Application.start()
+const application = Application.start();
+application.debug = false;
+window.Stimulus = application;
 
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+export { application };
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-    has_secure_password 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true, uniqueness: true
-  end
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+end
