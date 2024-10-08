8240fd3b52bfe6f485ac7316fe8caa252adcc89a
mariyador <aniknorod@gmail.com>
2024-10-08 19:50:29 -0400

    add logic who can edit amd delete items. logic for showing items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,54 +1,60 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!
-  before_action :find_item, only: [ :show, :edit, :update, :destroy ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
 
+  def index
+    @items = Item.all
+  end
 
-    def index
-      @items = Item.all
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    if @item.save
+      redirect_to @item, notice: "Item was successfully created."
+    else
+      render :new
     end
+  end
 
-    def new
-      @item = Item.new
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: "Item was successfully updated."
+    else
+      render :edit
     end
+  end
 
-    def edit
+  def destroy
+    @item.destroy
+    redirect_to items_path, notice: "Item was successfully deleted."
+  end
+
+  private
+
+  def find_item
+    @item = Item.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to items_path, alert: "Item not found."
+  end
+
+  def item_params
+    params.require(:item).permit(:title, :body, :condition, :location, :image)
+  end
+
+  def authorize_user!
+    unless @item.user_id == current_user.id
+      redirect_to items_path, alert: "You are not authorized to perform this action."
     end
-
-    def show
-    end
-
-    def create
-      @item = Item.new(item_params)
-      @item.user_id = current_user.id
-      if @item.save
-        redirect_to @item, notice: "Item was successfully created."
-      else
-        render :new
-      end
-    end
-
-    def update
-      if @item.update(item_params)
-        redirect_to @item, notice: "Item was successfully updated."
-      else
-        render :edit
-      end
-    end
-
-    def destroy
-      @item.destroy
-      redirect_to items_path
-    end
-
-    private
-
-    def find_item
-      @item = Item.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      redirect_to items_path, alert: "Item not found."
-    end
-
-    def item_params
-      params.require(:item).permit(:title, :body, :condition, :location, :image)
-    end
+  end
 end
